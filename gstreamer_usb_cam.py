d4571c3b2fff62c49c1a6668e38e00f3c333ca8c
sigruby <sigruby@mbp-2.local>
2025-08-06 13:29:43 +0900

    Update your_script.py and related files

--- gstreamer_usb_cam.py
+++ gstreamer_usb_cam.py
@@ -1,66 +1,40 @@
 #!/usr/bin/env python3
-import cv2
 import subprocess
 import signal
 
-def get_bitrate(width, height):
-    if width >= 3840 or height >= 2160:
-        return 10000
-    elif width >= 2560 or height >= 1440:
-        return 6000
-    elif width >= 1920 or height >= 1080:
-        return 20000 
-    elif width >= 1280 or height >= 720:
-        return 2500
-    elif width >= 640 or height >= 480:
-        return 1000
-    else:
-        return 800  # fallback for very low resolution
+import config as cfg
+
+TO_IP_LIST = [
+    {"to_id": 1, "to_ip": "10.100.30.21"},
+    {"to_id": 2, "to_ip": "10.100.30.22"},
+    {"to_id": 3, "to_ip": "10.100.30.23"},
+    {"to_id": 4, "to_ip": "10.100.30.24"},
+    {"to_id": 5, "to_ip": "10.100.30.25"},
+    {"to_id": 6, "to_ip": "10.100.30.26"},
+    {"to_id": 7, "to_ip": "10.100.30.27"},
+    {"to_id": 8, "to_ip": "10.100.30.28"}
+]
 
 def start_gstreamer(width, height, bitrate):
     gst_cmd = [
-        'gst-launch-1.0', 'fdsrc', '!',
-        'videoparse', f'width={width}', f'height={height}', 'format=rgb', 'framerate=30/1', '!',
-        'videoconvert', '!',
-        'x264enc', 'tune=zerolatency', f'bitrate={bitrate}', 'speed-preset=ultrafast', '!',
+        'gst-launch-1.0', '!',
+        'v4l2src', 'device=/dev/video2', '!',
+        'video/x-h264,width=1920,height=1080, framerate=30/1', '!',
         'rtph264pay', 'config-interval=1', 'pt=96', '!',
-        'udpsink', 'host=10.100.30.21', 'port=5000'
+        'udpsink', 'host=10.100.30.23', 'port=5000'
     ]
     return subprocess.Popen(gst_cmd, stdin=subprocess.PIPE)
 
 def main():
-#    cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
-    cap = cv2.VideoCapture(0)
-
-    # 원하는 해상도 설정
-    width = 1920
-    height = 1080
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    cap.set(cv2.CAP_PROP_FPS, 20)
-
-    # 비트레이트 자동 설정
-    bitrate = get_bitrate(width, height)
-    print(f"Using bitrate: {bitrate} kbps for resolution {width}x{height}")
-
-    gst_process = start_gstreamer(width, height, bitrate)
+    to_id = cfg.to_id
+    gst_proc = start_gstreamer(to_id)
 
     try:
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Camera read failed.")
-                break
+        gst_proc.wait()
 
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            gst_process.stdin.write(rgb_frame.tobytes())
-            gst_process.stdin.flush()  # 안정성 향상
     except KeyboardInterrupt:
-        pass
-    finally:
-        cap.release()
-        gst_process.send_signal(signal.SIGINT)
-        gst_process.wait()
+        gst_proc.send_signal(signal.SIGINT)
+        gst_proc.wait()
 
 if __name__ == '__main__':
     main()
