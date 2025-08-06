bd3bb3dc8ffb0aa5ac5ad2897b36a2a0f13dbca9
sigruby <sigruby@mbp-2.local>
2025-08-06 12:23:19 +0900

    Update your_script.py and related files

--- install_gstreamer.sh
+++ install_gstreamer.sh
@@ -14,14 +14,13 @@ sudo apt install -y \
   gstreamer1.0-plugins-good \
   gstreamer1.0-plugins-bad \
   gstreamer1.0-plugins-ugly \
-  gstreamer1.0-libav \
-  gstreamer1.0-doc \
   gstreamer1.0-x \
   gstreamer1.0-gl \
   gstreamer1.0-alsa \
   gstreamer1.0-pulseaudio \
   gstreamer1.0-qt5 \
-  gstreamer1.0-gtk3
+  gstreamer1.0-gtk3 \
+  gstreamer1.0-libav \
 
 echo "✅ GStreamer installation complete!"
 gst-launch-1.0 --version
