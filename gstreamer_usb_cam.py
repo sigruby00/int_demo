7127000e56978c7314819b83386d35b46d6e692d
sigruby <sigruby@mbp-2.local>
2025-08-06 13:34:01 +0900

    Update your_script.py and related files

--- gstreamer_usb_cam.py
+++ gstreamer_usb_cam.py
@@ -16,15 +16,18 @@ TO_IP_LIST = [
 ]
 
 def start_gstreamer(to_id):
-    to_ip = next((item['to_ip'] for item in TO_IP_LIST if item
-                     ['to_id'] == to_id), None)
+    to_ip = next((item['to_ip'] for item in TO_IP_LIST if item['to_id'] == to_id), None)
+    if to_ip is None:
+        raise ValueError(f"Invalid to_id: {to_id}")
+
     gst_cmd = [
-        'gst-launch-1.0', '!',
-        'v4l2src', 'device=/dev/video2', '!',
-        'video/x-h264,width=1920,height=1080, framerate=30/1', '!',
-        'rtph264pay', 'config-interval=1', 'pt=96', '!',
-        'udpsink', 'host={}'.format(to_ip), 'port=5000'
+        'gst-launch-1.0',
+        'v4l2src', 'device=/dev/video2',
+        '!', 'video/x-h264,width=1920,height=1080,framerate=30/1',
+        '!', 'rtph264pay', 'config-interval=1', 'pt=96',
+        '!', 'udpsink', f'host={to_ip}', 'port=5000'
     ]
+
     return subprocess.Popen(gst_cmd, stdin=subprocess.PIPE)
 
 def main():
