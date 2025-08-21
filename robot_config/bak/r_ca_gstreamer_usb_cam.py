299369c9f7a2d5be344eebbab886789d3279ce7a
sigruby00 <sigruby00@gmail.com>
2025-08-21 16:54:07 +0800

    reduce

--- robot_config/bak/r_ca_gstreamer_usb_cam.py
+++ robot_config/bak/r_ca_gstreamer_usb_cam.py
@@ -3,8 +3,20 @@ import subprocess
 import signal
 import config as cfg
 
+TO_IP_LIST = [
+    {"to_id": 2, "to_ip": "10.100.30.21"},
+    {"to_id": 3, "to_ip": "10.100.30.22"},
+    {"to_id": 4, "to_ip": "10.100.30.23"},
+    {"to_id": 5, "to_ip": "10.100.30.24"},
+    {"to_id": 6, "to_ip": "10.100.30.25"},
+    {"to_id": 7, "to_ip": "10.100.30.26"},
+    {"to_id": 8, "to_ip": "10.100.30.27"},
+    {"to_id": 9, "to_ip": "10.100.30.28"}
+]
+
+
 def start_gstreamer(to_id):
-    to_ip = next((item['to_ip'] for item in cfg.TO_IP_LIST if item['to_id'] == to_id), None)
+    to_ip = next((item['to_ip'] for item in TO_IP_LIST if item['to_id'] == to_id), None)
     if to_ip is None:
         raise ValueError(f"Invalid to_id: {to_id}")
 
@@ -19,7 +31,7 @@ def start_gstreamer(to_id):
     return subprocess.Popen(gst_cmd, stdin=subprocess.PIPE)
 
 def main():
-    to_id = cfg.to_id
+    to_id = 2
     gst_proc = start_gstreamer(to_id)
 
     try:
